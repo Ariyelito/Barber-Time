b31ccfd5f788814093d4ef5747cf8f022184e184
harfy345 <hakam.almotlk345@gmail.com>
2022-03-18 15:53:11 -0400

    Update Signup.js
    add user

--- src/views/Signup.js
+++ src/views/Signup.js
@@ -1,21 +1,35 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { View, Text, StyleSheet, TextInput , Alert } from 'react-native';
 import { textInputBackBorderColor, textInputBackgroundColor } from '../components/colors';
 import CustomButton from '../components/CustomButton';
 import { containerStyle } from '../components/variables';
+import { dropDatabaseUser, getAllUsers, insertUser } from '../manger/SqlManger';
 
 
-const Signup = () => {
+const Signup = ({navigation}) => {
+
+  const [email , setEmail] = useState('');
+  const [password , setPassword] = useState('');
+
+  const addUser = ()=>{
+    if(email !='' && password !=''){
+      insertUser(email , password);
+      Alert.alert('User was added'  );
+      navigation.navigate('Home');
+   
+
+    } else Alert.alert('Email and password are not valide');
+  };
   return (
     <View style={styles.container}>
 
 
       <Text>Barber Time</Text>
 
-      <TextInput style={styles.input} placeholder='Email'></TextInput>
-      <TextInput style={styles.input} placeholder='Password'></TextInput>
+      <TextInput style={styles.input} placeholder='Email' onChangeText={setEmail}></TextInput>
+      <TextInput style={styles.input} placeholder='Password' onChangeText={setPassword}></TextInput>
 
-      <CustomButton text={'Sign up'} onPress={() => { }}></CustomButton>
+      <CustomButton text={'Sign up'} onPress={() => { addUser(); }}></CustomButton>
 
 
     </View>
