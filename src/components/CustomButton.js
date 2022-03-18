3d6361da505aac4f15bef9a82d8d7dfe93bf24d4
harfy345 <hakam.almotlk345@gmail.com>
2022-03-18 14:39:51 -0400

    Update CustomButton.js

--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -16,14 +16,22 @@ export const CustomButton = ({ text , onPress }) => {
 const styles = StyleSheet.create({
   container: {
     alignItems: "center",
-    justifyContent: "center"
+    marginVertical:10,
   } , 
   
   button:{
-    
+    elevation: 8,
+    backgroundColor: "#009688",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
   },
   text : {
-
+    fontSize: 18,
+    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase"
   },
 })
 
