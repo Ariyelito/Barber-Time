f76cffaf1183142fba64f11f6e382cb0ee7ef432
harfy345 <hakam.almotlk345@gmail.com>
2022-03-19 13:58:20 -0400

    Update ProfileBarber.js

--- src/views/ProfileBarber.js
+++ src/views/ProfileBarber.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
+import { getAll } from '../manger/SqlManger';
 
 
 const ProfileBarber = ({route}) => {
   const barber = route.params.barber;
+
+  // const getAllAppoinments = ()=>{
+  //   getAll('appoinments',(tab)=>{
+  //     console.log(tab);
+  //   });
+  // }
   return (
     <View>
         <Text>Barber profile! {barber.email}</Text>
+        {/* ajouter un flat liste qui contient tous les rendez-vous */}
     </View>
   );
 }
