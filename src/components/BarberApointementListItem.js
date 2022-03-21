440bc72a18c21e63645d90289335bf1176bc8376
RAnas12 <56700993+RAnas12@users.noreply.github.com>
2022-03-21 16:10:24 -0400

    list apointement

---
+++ src/components/BarberApointementListItem.js
@@ -0,0 +1,45 @@
+//import liraries
+import React from 'react';
+import { View, Text, StyleSheet,TouchableOpacity } from 'react-native';
+import { containerStyle } from './variables';
+
+// create a component
+const BarberApointementListItem = ({time,name,email}) => {
+    return (
+        <View style={{flex:1}}>
+        <TouchableOpacity style={styles.container} >
+            <Text style={styles.name}>
+             {time}
+            </Text>
+            <Text style={styles.name}>
+             {name}
+            </Text>
+            <Text style={styles.name}>
+             {email}
+            </Text>
+        </TouchableOpacity>
+     </View>
+    );
+};
+
+// define your styles
+const styles = StyleSheet.create({
+    container: {
+        ...containerStyle ,
+        alignItems :'flex-start',
+        
+        paddingHorizontal:10,
+        marginVertical:3,
+        paddingVertical:10,
+        elevation:7,
+        borderBottomWidth:1,
+      } , 
+      name :{
+        fontSize:30,
+        
+      }, 
+    
+});
+
+//make this component available to the app
+export default BarberApointementListItem;
