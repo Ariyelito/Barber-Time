cbd5311046ee941b369474191ae87dc90f1584ee
OrlandoAriel <orlandovillanueva11@gmail.com>
2022-03-24 19:15:45 -0400

    blbab

--- src/views/ListAppoinments.js
+++ src/views/ListAppoinments.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Text,View, FlatList, StyleSheet } from 'react-native';
+import { Text, View, FlatList, StyleSheet } from 'react-native';
 import BarberApointementListItem from '../components/BarberApointementListItem';
 import { containerStyle } from '../components/variables';
 import { getAll } from '../db/SqlManager';
@@ -7,47 +7,48 @@ import { getAll } from '../db/SqlManager';
 
 const ListAppoinments = () => {
 
- 
-  const [data , setData] = useState([]);
 
-  useEffect(()=>{
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
     //getAll('barbers', tab=>console.log(tab) );
-    getAll('appoinments', tab=>setData(tab) );
-  },[]);
- 
-
-const renderItem=({item})=>{
-console.log(item);
+    getAll('appoinments', tab => setData(tab));
+  }, []);
 
 
-return(
-  <BarberApointementListItem time={item.time} name={item.nameClt} email={item.emailClt} onPressHandler={()=>{console.log("remove apointement")}} />
+  const renderItem = ({ item }) => {
+    console.log(item);
 
-);
 
-}
+    return (
+      <BarberApointementListItem time={item.time} name={item.nameClt} email={item.emailClt} onPressHandler={() => { console.log("remove apointement") }} />
+
+    );
+
+  }
 
 
   return (
-    
-         
- <FlatList
-    style={styles.flatList}
-    data={data} 
-    renderItem={renderItem}
-    ></FlatList>
-   
-   
+    <View>
+      {data.length != 0 ?
+      <FlatList
+        style={styles.flatList}
+        data={data}
+        renderItem={renderItem}
+      ></FlatList>
+     : <Text>Vous etes pas fresh</Text>}
+    </View>
+
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    ...containerStyle ,
-  } , 
-  flatList:{
-    alignSelf:'stretch',
-  
+    ...containerStyle,
+  },
+  flatList: {
+    alignSelf: 'stretch',
+
   }
 
 })
