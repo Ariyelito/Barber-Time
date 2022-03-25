759cdfccac13e6d37499d9b4c6fc27a1ae5c5c1f
RAnas12 <56700993+RAnas12@users.noreply.github.com>
2022-03-25 16:25:06 -0400

    Revert "Merge branch 'navigation-drawer-new'"
    This reverts commit 6b9598768666fecff30906b88a64e71fbb999712, reversing
    changes made to 9d04dcabc08cebe133e2c6260f248adeda624f53.

--- src/views/ProfileBarber.js
+++ src/views/ProfileBarber.js
@@ -1,15 +1,9 @@
-import 'react-native-gesture-handler';
 import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 import { useSelector } from 'react-redux';
 import { getAll } from '../db/SqlManager';
 import ListAppoinments from './ListAppoinments';
 
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
-import SettingScreen from './SettingScreen';
-
-const Drawer = createDrawerNavigator();
 
 const ProfileBarber = () => {
   const barber = useSelector(state => state.barber.connected);
@@ -23,17 +17,8 @@ const ProfileBarber = () => {
      });
    }
   return (
-
-    <><NavigationContainer>
-      <Drawer.Navigator initialRouteName="ProfileBarber">
-        <Drawer.Screen name="Appointments" component={ListAppoinments} />
-        <Drawer.Screen name="Settings" component={SettingScreen} />
-      </Drawer.Navigator>
-    </NavigationContainer>
-    
-    <ListAppoinments /></>
-
-    
+   
+    <ListAppoinments />
   );
 }
 
