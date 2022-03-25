9d04dcabc08cebe133e2c6260f248adeda624f53
harfy345 <hakam.almotlk345@gmail.com>
2022-03-24 22:29:56 -0400

    sorry

--- src/components/LocalStorage.js
+++
@@ -1,9 +0,0 @@
-export const saveLogin = async (activeBarber) => {
-    try {
-      await AsyncStorage.setItem('login', JSON.stringify(activeBarber))
-    } catch (e) {
-      console.log('error while saving login info' + e)
-    }
-  }
-
-
