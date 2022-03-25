6a3fcac1b0119d3d3264b89a62be6beeb42a759a
Onibaku3 <patrick_al-mondalek3@hotmail.com>
2022-03-24 21:56:21 -0400

    navigation

--- src/views/ProfileBarber.js
+++ src/views/ProfileBarber.js
@@ -4,6 +4,11 @@ import { useSelector } from 'react-redux';
 import { getAll } from '../db/SqlManager';
 import ListAppoinments from './ListAppoinments';
 
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import SettingScreen from './SettingScreen';
+
+const Drawer = createDrawerNavigator();
 
 const ProfileBarber = () => {
   const barber = useSelector(state => state.barber.connected);
@@ -17,7 +22,17 @@ const ProfileBarber = () => {
      });
    }
   return (
-    <ListAppoinments />
+
+    <><NavigationContainer>
+      <Drawer.Navigator initialRouteName="ProfileBarber">
+        <Drawer.Screen name="Appointments" component={ListAppoinments} />
+        <Drawer.Screen name="Settings" component={SettingScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+    
+    <ListAppoinments /></>
+
+    
   );
 }
 
